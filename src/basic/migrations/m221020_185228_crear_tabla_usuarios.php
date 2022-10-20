bd8bc88ed9a80eb480aa101bfbccd9731fbd3fa2
dario <cochocossolidario@gmail.com>
2022-10-20 16:18:24 -0300

    Punto  4 creacion de tabla

---
+++ src/basic/migrations/m221020_185228_crear_tabla_usuarios.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m221020_185228_crear_tabla_usuarios
+ */
+class m221020_185228_crear_tabla_usuarios extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('usuario', [
+            'username' => $this->primaryKey()->notNull(),
+            'name' => $this->string(80)->notNull(),
+            'passwword' => $this->integer(8),
+            'authKey' => $this->integer(8),
+            'accesToken' => $this->integer(8),
+        ]);   
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('usuario');;
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m221020_185228_crear_tabla_usuarios cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
