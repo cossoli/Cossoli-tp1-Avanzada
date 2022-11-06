bf41dda39d9c2565317a0377edcbb4d16149be9a
dario <cochocossolidario@gmail.com>
2022-11-06 20:47:27 -0300

    punto 4  y 5

--- src/basic/models/Usuario.php
+++ src/basic/models/Usuario.php
@@ -14,7 +14,8 @@ use Yii;
  * @property int|null $accesToken
  * @property string|null $apellido
  */
-class Usuario extends \yii\db\ActiveRecord
+class Usuario extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
+
 {
     /**
      * {@inheritdoc}
@@ -52,4 +53,39 @@ class Usuario extends \yii\db\ActiveRecord
             'apellido' => 'apellido',
         ];
     }
+
+    public static function findIdentity($id)
+    {
+        return self::findOne($id);
+    }
+     
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return self::findOne(['accesToken'=>$token]);
+    }
+    
+    public static function findbYUsername ($username)
+    {
+       return self::findOne(['username'=>$username]);    
+
+    }
+    public function getId()
+    {
+        return $this->id;
+
+    }
+    public function getAuthKey()
+    {
+        return $this->authkey;
+    }
+    
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey ===$authKey;
+    } 
+    public function validatePassword($password)
+    {
+        return password_verify($password, $this->passwword);
+    }
+
 }
